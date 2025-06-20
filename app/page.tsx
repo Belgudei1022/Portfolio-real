d086c2909562e7fe9b4bbe336270ebea815375c6
Belgudei1022 <belgudei379@gmail.com>
2025-06-20 20:19:30 +0800

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,37 @@
+'use client'
+
 import Spline from "@splinetool/react-spline";
 import HeroSection from "@/components/hero/page";
 import AboutSection from "@/components/about/page";
+import ExperienceSection from "@/components/experience/page";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let scrollInstance: any;
+
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+
+      if (scrollRef.current) {
+        scrollInstance = new LocomotiveScroll({
+          el: scrollRef.current,
+          smooth: true,
+          lerp: 0.05, // lower = smoother (default is 0.1)
+          multiplier: 1, // scroll speed (default is 1)
+        });
+      }
+    })();
+
+    return () => {
+      if (scrollInstance) {
+        scrollInstance.destroy();
+      }
+    };
+  }, []);
+
   return (
     <main className="relative w-full min-h-screen overflow-x-hidden bg-black">
       <Spline
@@ -12,9 +41,14 @@ export default function Home() {
 
       <div className="fixed inset-0 bg-black opacity-40 z-10"></div>
 
-      <div className="relative z-20 w-full">
-          <HeroSection />
-          <AboutSection/>
+      <div
+        ref={scrollRef}
+        data-scroll-container
+        className="min-h-auto relative z-20 w-full flex flex-col gap-[100px]"
+        
+      >
+        <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
---
+++ components/experience/page.tsx
@@ -0,0 +1,9 @@
+import gsap from "gsap"
+
+export default function ExperienceSection(){
+    return (
+        <section className="w-full h-screen flex flex-row justify-center items-center relative">
+
+        </section>
+    )
+}
